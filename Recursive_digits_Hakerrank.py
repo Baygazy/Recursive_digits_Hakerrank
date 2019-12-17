495bb81577e66238c524f80e4259bfe90c945372
Baygazy <56838916+Baygazy@users.noreply.github.com>
2019-12-17 12:14:54 +0600

    Update Recursive_digits_Hakerrank.py

--- Recursive_digits_Hakerrank.py
+++ Recursive_digits_Hakerrank.py
@@ -24,33 +24,3 @@ result = superDigit(n, k)
 print(result)
 
 
-
-
-
-
-
-# def superDigit(n, k):
-#     str_li = n * k
-#     # return len(str_li)
-#     li = [int(i) for i in str_li]
-#     sum_n = sum(li)
-#     while len(str(sum_n)) != 1:
-#         li = [int(i) for i in str(sum_n)]
-#         sum_n = sum(li)
-#     return sum_n
-#
-# nk = input('Enter code: ').split()
-#
-# n = nk[0]
-#
-# k = int(nk[1])
-#
-# result = superDigit(n, k)
-# print(result)
-
-
-
-
-# arr1 = [1,2,3,4,5]
-# x = functools.reduce(lambda x,y: x+y,arr1)
-# print(x)
